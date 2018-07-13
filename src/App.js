ac54dfb15bce7a962af6dadb7e16f33a827ec4cb
Unknown <fofonso_97@me.com>
2018-07-13 12:59:26 -0600

    Added menu and stats overlook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Col, Collapse, Container, Nav, Navbar, NavbarToggler, NavbarBrand, NavItem, NavLink, Row } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  state = {
+    collapsed: true
+  }
+
+  toggleNavbar = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <Navbar color="faded" light>
+            <NavbarBrand href="/" className="mr-auto" style={{ color: '#0065ff' }}>Zubut</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="#">Inicio</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#">Dashboard</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Row className="resumen">
+            <Col>
+              <div className="resumen-item">
+                <p>633</p>
+                <span>Viajes</span>
+              </div>
+            </Col>
+            <Col>
+              <div className="resumen-item">
+                <p>$28K</p>
+                <span>Ganancias</span>
+              </div>
+            </Col>
+            <Col>
+              <div className="resumen-item">
+                <p>2.3K</p>
+                <span>Kms</span>
+              </div>
+            </Col>
+            <Col>
+              <div className="resumen-item">
+                <p>701</p>
+                <span>Articulos</span>
+              </div>
+            </Col>
+            <Col>
+              <div className="resumen-item">
+                <p>9/10</p>
+                <span>Rating</span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
