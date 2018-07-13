275a546a221a01817ad8fcb0fac4694ca1a0065e
Unknown <fofonso_97@me.com>
2018-07-13 16:19:42 -0600

    Added distances chart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { Col, Collapse, Container, Nav, Navbar, NavbarToggler, NavbarBrand, NavItem, NavLink, Row } from 'reactstrap';
-import { PieChart, Pie, Tooltip } from 'recharts';
+import { CartesianGrid, Legend, Line, LineChart, PieChart, Pie, Tooltip, XAxis, YAxis } from 'recharts';
 import './App.css';
 
 const data02 = [{ name: 'Completados', value: 603 }, { name: 'Cancelados', value: 30 }];
+
+const distanciasData = [
+  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+];
 
 class App extends Component {
   state = {
@@ -84,8 +94,23 @@ class App extends Component {
 
             {/* Distancias */}
             <Col xs="12" md="6" className="section distancias">
-              <div></div>
+              <div>
+                <span className="cifra">2300 kilometros recorridos</span>
+                <div>
+                  <LineChart width={500} height={150} data={distanciasData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                  </LineChart>
+                </div>
+              </div>
             </Col>
+
           </Row>
         </Container>
       </div>
